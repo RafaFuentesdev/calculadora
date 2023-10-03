8bcdc1175e73abc9e6ab687b9e5c75840f5cae42
Rafael Fuentes Cantero <rafafuentesdev@gmail.com>
2023-10-04 00:37:44 +0200

    calculadora 3

---
+++ 3-calculadora.py
@@ -0,0 +1,50 @@
+from simple_term_menu import TerminalMenu
+
+
+def obtener_numeros():
+    num1 = float(input("Introduce el primer número: "))
+    num2 = float(input("Introduce el segundo número: "))
+    return num1, num2
+
+
+def sumar(num1, num2):
+    return num1 + num2
+
+
+def restar(num1, num2):
+    return num1 - num2
+
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+
+def dividir(num1, num2):
+    return num1 / num2
+
+
+def salir():
+    print("¡Hasta luego!")
+    exit()
+
+
+def main():
+    opciones = ["Sumar", "Restar", "Multiplicar", "Dividir", "Salir"]
+    funciones = [sumar, restar, multiplicar, dividir, salir]
+
+    while True:
+        menu = TerminalMenu(opciones, clear_screen=True)
+        indice_seleccionado = menu.show()
+        if indice_seleccionado != len(opciones) - 1:  # Si la opción no es 'Salir'
+            print(f"{opciones[indice_seleccionado]}")
+            num1, num2 = obtener_numeros()
+            funcion_seleccionada = funciones[indice_seleccionado]
+            resultado = funcion_seleccionada(num1, num2)  # Almacenar el resultado
+            print(f"El resultado es: {resultado}")
+            input("Pulsa ENTER para continuar...")
+        else:
+            salir()
+
+
+if __name__ == "__main__":
+    main()
